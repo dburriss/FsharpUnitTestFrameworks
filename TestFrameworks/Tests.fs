e1bb23d56bb7005fd708c46b03112c2c295e0865
dburriss <devon@chimplab.co>
2018-12-06 08:47:29 +0100

    First FsUnit test

--- TestFrameworks/Tests.fs
+++ TestFrameworks/Tests.fs
@@ -11,3 +11,16 @@ module XUnitTests =
         let expected = "info@acme.com"
         let actual = toEmail name
         Assert.Equal (expected, actual)
+
+module FsUnitTests =
+
+    open Xunit
+    open FsUnit.Xunit
+    open Email
+
+    [<Fact>]
+    let ``toEmail with null gives info [at] acme [dot] com`` () =
+        let name = null
+        let expected = "info@acme.com"
+        let actual = toEmail name
+        actual |> should equal expected
